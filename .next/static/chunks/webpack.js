5c484af81b502eff95dc7093a7b7687c1d4cd7e2
Validatrium <validatrium@gmail.com>
2023-10-04 15:22:11 +0300

    added juno mainnet endpoints

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f35233dfeee70d93"; }
+/******/ 		__webpack_require__.h = function() { return "d8a1ad3ccd3a9a8e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
